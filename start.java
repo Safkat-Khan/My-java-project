58307eb17b3fee7a2bf2312cbdd45a9d753fbbfb
Safkat khan Tafsir <safkatkhan420@gmail.com>
2024-05-20 23:51:36 +0600

    All Updated

--- start.java
+++ start.java
@@ -1,11 +1,11 @@
 
 public class Start {
   public static void main(String[] args) {
-   //  new LogInPage();
+     new LogInPage();
   //  new ForgetPass();
   //  new Signup();
   //  new AdminLogin();
-    new Profile("Test");
+  //  new Profile("Test");
   //  new AdminDashboard("Test");
   //  new UserdashBoard();
   //  new Payment(); 
